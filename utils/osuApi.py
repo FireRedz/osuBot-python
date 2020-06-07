896cf4f37b39fb8c1f7f63f7bf85888be526deab
FireRedz <asiagamingz122@gmail.com>
2020-06-07 17:56:03 +0800

    change getBeatmap to work with storage for faster speed

--- utils/osuApi.py
+++ utils/osuApi.py
@@ -3,13 +3,20 @@ from conf import config
 from utils.storage import storage
 
 apikey = config.apiKey
-baseURL = f'https://osu.ppy.sh/api/get_beatmaps?k={apikey}&b=[shit]'
+baseURL = [f'https://osu.ppy.sh/api/get_beatmaps?k={apikey}&b=[shit]']
 
 def getBeatmap(mapID):
+    if mapID in storage.mapData: # check if map exist in storage
+        #print('exist!')
+        tempReq = storage().getData(mapID)
+        return tempReq
+
     try:
-        tempReq = r.get(baseURL.replace('[shit]', mapID)).json()[0]
+        #print(baseURL[0].replace('[shit]', mapID))
+        tempReq = r.get(baseURL[0].replace('[shit]', mapID)).json()[0]
+        storage().insertData(mapID, tempReq) # insert map data into storage
     except:
         tempReq = None
-    
+
     return tempReq
 
